f90344688f439e42f0f8866f3964737b0b6a1a63
sseerrnn <sern2001@hotmail.com>
2023-02-06 16:46:48 +0700

    Connect with mongodb atlas

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const hospitals = require(`./routes/hospitals`);
+const connectDB = require("./config/db");
+//load env vars
+dotenv.config({ path: "./config/config.env" });
 
+//connect to database
+connectDB();
 
-dotenv.config({ path: './config/config.env' });
+const app = express();
 
-const app=express();
+// body parser
+app.use(express.json());
 
-app.use('/api/v1/hospitals',hospitals);
+app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,console.log('Server running in '+process.env.NODE_ENV+' mode on port '+PORT));
 
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in " + process.env.NODE_ENV + " mode on port " + PORT
+  )
+);
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server & exit process
+  server.close(() => process.exit(1));
+});
