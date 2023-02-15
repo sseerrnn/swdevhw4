daf77f936762269b516327f6f82b438988bd414c
sseerrnn <sern2001@hotmail.com>
2023-02-15 22:57:39 +0700

    Add Authentication

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const hospitals = require(`./routes/hospitals`);
+const auth = require(`./routes/auth`);
 const connectDB = require("./config/db");
 //load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -13,7 +15,11 @@ const app = express();
 // body parser
 app.use(express.json());
 
+//cookie parser
+app.use(cookieParser());
+
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
